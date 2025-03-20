4b8bf4f6befed32fbb7164f44e87e885f8059438
levchig737 <levchekarev3@gmail.com>
2025-03-20 19:05:03 +0300

    fix templates: fix custom-control-label Случайные тексты disable

--- research/text_generator/urls.py
+++ research/text_generator/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('generator/', views.text_generator_view, name='text_generator_form'),
+    path('texts/', views.get_texts_for_list, name='text_generator_texts'),
 ]
--- research/text_generator/views.py
+++ research/text_generator/views.py
@@ -2,7 +2,7 @@ import logging
 import random
 
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from text_app.models.tbl_textlist import TblTextListDescription
@@ -446,3 +446,9 @@ def text_generator_view(request: HttpRequest) -> HttpResponse:
             return render(request, "text_generator/form_by_code.html", context={
                 "mode": mode,
             })
+
+def get_texts_for_list(request):
+    list_id = request.GET.get('list_id')
+    text_list = TblTextListDescription.get_item(request.user, int(list_id))
+    texts = [{'id': item.text.id, 'title': item.text.title} for item in text_list.items]
+    return JsonResponse({'texts': texts})
